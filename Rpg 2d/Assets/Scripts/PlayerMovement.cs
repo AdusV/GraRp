47fcc619968227fa5aba6cf10a7dcb605b555c44
AdusV <adriankomendera@interia.pl>
2020-05-15 09:36:25 +0200

    Start Comit

---
+++ Rpg 2d/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private void Update()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+        float vDirection = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(hDirection, vDirection) * speed);
+    }
+}
