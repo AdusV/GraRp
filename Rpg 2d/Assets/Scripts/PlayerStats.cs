59bc0bb667759a5c04f19534e85eafd3d6b60bf3
AdusV <adriankomendera@interia.pl>
2020-05-15 21:14:26 +0200

    Zarys

---
+++ Rpg 2d/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] private int breathingTime;
+    [SerializeField] private int maxBreathingTime;
+
+    [SerializeField] private int regenerationRate;
+
+    public void Regeneration()
+    {
+        breathingTime += regenerationRate;
+    }
+}
